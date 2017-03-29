688ce8b8c7f57c903a86b1be56c85081016df35d
Clark Feeley <Caf72@txstate.edu>
2017-03-29 18:26:12 -0500

    Update FTP_Main.py

--- FTP_Main.py
+++ FTP_Main.py
@@ -2,11 +2,25 @@
 import FTP_fileUpload
 import FTP_fileDelete
 
-SERVER = ""
-USERNAME = ""
-PASSWORD = ""
-filename = ""
+def getserver_address():
+    print("Enter the FTP server's url you want to connect to.")
+    SERVER = input()
+    return SERVER
 
-FTP_fileUpload.fileUpload(filename, SERVER, USERNAME, PASSWORD)
-FTP_fileDelete.fileDelete(filename, SERVER, USERNAME, PASSWORD)
+def getserver_username():
+    print("Enter the FTP's username.")
+    USERNAME = input()
+    return USERNAME
+
+def getserver_password():
+    print("Enter the FTP's password.")
+    PASSWORD = input()
+    return PASSWORD
+
+
+filename = "test.txt"
+
+
+FTP_fileUpload.fileUpload(filename, getserver_address(), getserver_username(), getserver_password())
+FTP_fileDelete.fileDelete(filename, getserver_address(), getserver_username(), getserver_password())
 
