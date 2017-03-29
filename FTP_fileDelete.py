25ee3e240d503f6c5934377252e09eabea67bacc
Clark Feeley <Caf72@txstate.edu>
2017-03-29 17:25:52 -0500

    Update FTP_fileDelete.py

--- FTP_fileDelete.py
+++ FTP_fileDelete.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 from ftplib import FTP
 
-def fileDelete():
+def fileDelete(filename):
     ftp = FTP("SERVER")
     ftp.login("USERNAME", "PASSWORD")
     ftp.cwd('/data/')
     #ftp.retrlines('LIST')
-    ftp.delete("test.txt")
+    ftp.delete("filename")
     ftp.quit()
 
-fileDelete()
+fileDelete(filename)
