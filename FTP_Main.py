95e6be4532743ee9af744b20b44c3c6e2d217ce5
Clark Feeley <Caf72@txstate.edu>
2017-03-29 20:10:04 -0500

    Update FTP_Main.py

--- FTP_Main.py
+++ FTP_Main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 import FTP_fileUpload
 import FTP_fileDelete
+import os
+
 
 def getserver_address():
     print("Enter the FTP server's url you want to connect to.")
@@ -17,10 +19,25 @@ def getserver_password():
     PASSWORD = input()
     return PASSWORD
 
+def getfile_upload():
+    fileDir = os.path.dirname(os.path.realpath('__file__'))
 
-filename = "test.txt"
+    # For accessing the file in the same folder
+    filename = "test.txt"
+
+    # For accessing the file in a folder contained in the current folder
+    # filename = os.path.join(fileDir, 'test_folder/test.txt')
+
+    # For accessing the file in the parent folder of the current folder
+    # filename = os.path.join(fileDir, '../same.txt')
+    # readFile(filename)
+
+    # For accessing the file inside a sibling folder.
+    # filename = os.path.join(fileDir, '../sibling_test_folder/test.txt')
+    # filename = os.path.abspath(os.path.realpath(filename))
+    
+    return filename
 
 
-FTP_fileUpload.fileUpload(filename, getserver_address(), getserver_username(), getserver_password())
-FTP_fileDelete.fileDelete(filename, getserver_address(), getserver_username(), getserver_password())
-
+FTP_fileUpload.fileUpload(getfile_upload(), getserver_address(), getserver_username(), getserver_password())
+FTP_fileDelete.fileDelete(getfile_upload(), getserver_address(), getserver_username(), getserver_password())
