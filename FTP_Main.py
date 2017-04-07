b96150e327729c01628aa930786fd5a7aed3a040
Clark Feeley <Caf72@txstate.edu>
2017-04-07 08:20:47 -0500

    Add files via upload

--- FTP_Main.py
+++ FTP_Main.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-import FTP_fileUpload
-import FTP_fileDelete
+import FTP_Library
 import os
 
 
@@ -23,21 +22,28 @@ def getfile_upload():
     fileDir = os.path.dirname(os.path.realpath('__file__'))
 
     # For accessing the file in the same folder
-    filename = "test.txt"
+    #file_upload = "test.txt"
+
+    chosen_file = input('What is the name of the file you want to upload')
 
     # For accessing the file in a folder contained in the current folder
-    # filename = os.path.join(fileDir, 'test_folder/test.txt')
+    file_upload = os.path.join(fileDir, 'test_folder/' + chosen_file)
 
     # For accessing the file in the parent folder of the current folder
-    # filename = os.path.join(fileDir, '../same.txt')
+    #filename = os.path.join(fileDir, '../same.txt')
     # readFile(filename)
 
     # For accessing the file inside a sibling folder.
     # filename = os.path.join(fileDir, '../sibling_test_folder/test.txt')
     # filename = os.path.abspath(os.path.realpath(filename))
-    
+
+    return file_upload
+
+def getfile_pull():
+    filename = input('Which directory is the file you want to pull in?')
     return filename
 
-
-FTP_fileUpload.fileUpload(getfile_upload(), getserver_address(), getserver_username(), getserver_password())
-FTP_fileDelete.fileDelete(getfile_upload(), getserver_address(), getserver_username(), getserver_password())
+def main():
+    FTP_Library.fileUpload(getfile_upload(), getserver_address(), getserver_username(), getserver_password())
+    FTP_Library.fileDelete(getfile_upload(), getserver_address(), getserver_username(), getserver_password())
+    FTP_Library.filePull(getfile_pull(), getserver_address(), getserver_username(), getserver_password())
